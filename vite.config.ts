7ffdda4a139186a6e82069fd0d239837de11f0d4
fitcapital <joel@fitcapital.com>
2026-05-31 21:55:46 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    target: "vercel",
+    target: "node-server",
   },
 });
